326dd27bfc4c4cd69893af732694d1c2094e757e
nayoung <nayoungace@naver.com>
2025-11-27 15:37:06 +0900

    최초 커밋

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">Sample-api-project</p>
+        </div>
+      </main>
+    </div>
+  );
+}
